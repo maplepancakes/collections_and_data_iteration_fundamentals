a5a0fa6a2cca319ce31fd790fcd7b87e586edb19
maplepancakes <ngsiohongssg@gmail.com>
2025-07-18 23:13:34 +0800

    Added comment for recursion practice

--- RecursionPractice.cs
+++ RecursionPractice.cs
@@ -1,5 +1,9 @@
 namespace collections_and_data_iteration_fundamentals;
 
+/*
+ * Things to remember: -
+ * - Any code placed before recursion will occur before recursion (e.g. PrintNumbersFromNToOne), and any code placed after recursion will only occur after the last recursion point has been reached (e.g. PrintNumbersFromOneToN)
+ */
 public class RecursionPractice
 {
     public void PrintNumbersFromOneToN(int n)
